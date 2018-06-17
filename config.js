bb4422436bdbc54af63f957259ba1917699126b3
sebseb7 <sebgreenbus@gmail.com>
2018-06-17 19:50:52 +0200

    upd

--- config.js
+++ config.js
@@ -2,14 +2,13 @@ var api = "/api";
 
 var api_blockexplorer = "https://explorer.ipbc.io:8120";
 
-var poolHost = "support.ipbc.io";
+var poolHost = "mining.bit.tube";
 
 var irc = "irc.freenode.net/#ipbc";
 
 var email = "hello@ipbc.io";
 
-var cryptonatorWidget = ["IPBC-BTC", "IPBC-USD", "IPBC-EUR"];
-//var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
@@ -20,20 +19,15 @@ var transactionExplorer = "/?hash={id}#blockchain_transaction";
 var themeCss = "themes/default-theme.css";
 
 var networkStat = {
-  "ipbc": [
+  "tube": [
 	["pool.ipbc.io", "pool_ipbc_io"],
 	["support.ipbc.io", "support_ipbc_io"],
 	["support2.ipbc.io", "support2_ipbc_io"],
-	["ipbc.fatpanda.club", "ipbc_fatpanda_club"],
 	["cryptoknight.cc/ipbc", "cryptoknight_cc"],
+	["ipbc.iccob.com","ipbc_iccob_com"],
 	["youpool.io", "youpool_io"],
-	["ipbc.minez.ro","ipbc_minez_ro"],
 	["pool.ipbc.io","pool_ipbc_io"],
-	["ipbccoin.newpool.pw","ipbccoin_newpool_pw"],
 	["ipbc.bathmatminingpools.com","ipbc_bathmatminingpools_com"],
-	["ipbc.pool.gntl.co.uk","ipbc_pool_gntl_co_uk"],
-	["ipbcpool.supportcryptonight.com","ipbcpool_supportcryptonight_com"],
-	["ipbc.semipool.com","ipbc_semipool_com"],
-	["ipbc.iccob.com","ipbc_iccob_com"]
+	["ipbc.semipool.com","ipbc_semipool_com"]
   ]
 };
